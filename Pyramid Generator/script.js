540e8f9ce29ed6e4dfd6e9827f1d03614d720c7d
dyerman456 <dyerman456@gmail.com>
2024-05-06 10:32:36 +0200

    test-push-and-pop

--- Pyramid Generator/script.js
+++ Pyramid Generator/script.js
@@ -1,4 +1,8 @@
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
-console.log(rows[0]);
+let pushed = rows.push("freeCodeCamp");
+console.log(pushed);
+let popped = rows.pop();
+console.log(popped);
+console.log(rows);
